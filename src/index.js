24759e17837245097aab3d0fb5c8c79a43207786
Uladzislau Sipaila <UladzislauS@gmail.com>
2017-04-06 03:25:01 +0300

    Added merge implementation

---
+++ src/index.js
@@ -0,0 +1,16 @@
+module.export = function merge(first, second) {
+    // creating Object instances for primitives
+    let firstObject = new Object(first);
+    let secondObject = new Object(second);
+    
+    // copying initial object to protect from changing 
+    let mergeObject = firstObject === first ? merge(null, firstObject) : firstObject;   
+
+    for (let key in secondObject) {
+        mergeObject[key] = mergeObject[key] instanceof Object && secondObject[key] instanceof Object
+            ? merge(mergeObject[key], secondObject[key])
+            : secondObject[key];
+    }
+
+    return mergeObject;
+}
